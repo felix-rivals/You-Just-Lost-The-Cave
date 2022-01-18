95eaf05b6a202c299d22dab4add91aa4a85b1e09
Loic-Noctal <noctaloup@gmail.com>
2022-01-18 16:44:40 +0100

    Ajout un peu de map

--- Perdu Express CGJ/Assets/PlayerScript.cs
+++ Perdu Express CGJ/Assets/PlayerScript.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-    public float speed;
+    public float moveSpeed;
     public float jumpHeight;
+
+    private float dashSpeed = 6f; // la vitesse du dash
+    private float activeSpeed; // stockera la vitesse de base OU du dash selon si la touche shift est enfoncée ou non
 
     private Rigidbody2D rb;
     private float moveInput;
@@ -14,29 +15,69 @@ public class PlayerScript : MonoBehaviour {
     private bool isGrounded = true;
 
     private int jumps;
-    private bool canDoubleJump = false; // pour plus tard : si on a débloqué le double saut ou pas
-    
+    public bool canDoubleJump = true; // pour plus tard : si on a débloqué le double saut ou pas
 
-    void Start() {
+    public float dashLength = .1f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+        activeSpeed = moveSpeed;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput * activeSpeed, rb.velocity.y);
     }
 
-    void Update() {
+    void Update()
+    {
 
         if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || jumps > 0)) // Si la touche espace est enfoncée & qu'il est sur le sol OU qu'il lui reste un double saut
         {
             Jump();
             jumps--;
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (moveInput!=0))
+        {
+            
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                jumps = 0;
+                rb.velocity = Vector3.zero;
+                rb.gravityScale = 0f;
+                activeSpeed = moveSpeed * dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter < 0)
+            {
+                rb.gravityScale = 2.8f;
+                activeSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+
     }
 
-    void Jump() {
+    void Jump()
+    {
         rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
     }
 
@@ -44,16 +85,22 @@ public class PlayerScript : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D col)
     {
         isGrounded = true;
-        if (canDoubleJump) {
+        if (canDoubleJump)
+        {
             jumps = 2;
-        } else {
+        }
+        else
+        {
             jumps = 1;
         }
     }
 
     void OnCollisionExit2D(Collision2D col)
     {
+        if (jumps == 2)
+        {
+            jumps--;
+        }
         isGrounded = false;
     }
-
 }
